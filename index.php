c9b58793e2ab8d8683fb8cae8be8a575ecfac25d
Invader745 <RISH.TIWARI745@GMAIL.COM>
2024-02-05 13:27:54 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+$insert = false;
+if(isset($_POST['source'])){
+    // Set connection variables
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+
+    // Create a database connection
+    $con = mysqli_connect($server, $username, $password);
+
+    // Check for connection success
+    if(!$con){
+        die("connection to this database failed due to" . mysqli_connect_error());
+    }
+    // echo "Success connecting to the db";
+
+    // Collect post variables
+    $source = $_POST['source'];
+    $destination = $_POST['destination'];
+    
+    $sql = "INSERT INTO `mydb`.`ticket` (`source`, `destination`,`dt`) VALUES ('$source', '$destination', current_timestamp());";
+    // echo $sql;
+
+    // Execute the query
+    if($con->query($sql) == true){
+        // echo "Successfully inserted";
+
+        // Flag for successful insertion
+        $insert = true;
+    }
+    else{
+        echo "ERROR: $sql <br> $con->error";
+    }
+
+    // Close the database connection
+    $con->close();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Travel Form</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Sriracha&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <img class="bg" src="bg.jpg" alt="IIT Kharagpur">
+    <div class="container">
+        <h1>online ticketing system</h3>
+        <p>Enter source and destination </p>
+        <?php
+        if($insert == true){
+        echo "<p class='submitMsg'>Thanks for submitting your form. We are happy to see you joining us for the US trip</p>";
+        }
+    ?>
+        <form action="index.php" method="post">
+            <input type="text" name="source" id="source" placeholder="Enter source">
+            <input type="text" name="destination" id="destination" placeholder="Enter destination">
+            <button class="btn">Submit</button> 
+        </form>
+    </div>
+    <script src="index.js"></script>
+    
+</body>
+</html>
